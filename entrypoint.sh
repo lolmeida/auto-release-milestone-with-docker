82597c51a46c2154be40db619559f667d4d6bf1a
lolmeida <lolmeida@gmail.com>
2024-04-28 16:00:30 +0100

    add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# set -e // exit when any command fails
+# set -x // print each command before executing
+# set -o pipefail // exit when any command in a pipe fails
+# set -u // exit when using an undefined variable
+
+# exit when any command fails, print each command before executing, exit when using an undefined variable
+set -e -x -u 
+
+# set the output variable release-url to http://example.com
+echo "::set-output name=release-url::http://example.com" 
+
+# exit with a successful status code
+exit 0 
