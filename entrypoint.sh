e762d547561da12f823fe6cf714ef39001a6ba1f
lolmeida <lolmeida@gmail.com>
2024-04-28 16:49:46 +0100

    added workfloes/test.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,14 @@
 # set -u // exit when using an undefined variable
 
 # exit when any command fails, print each command before executing, exit when using an undefined variable
-# set -e -x -u 
+set -e -x -u 
 
-set -u // exit when using an undefined variable
+# set -u // exit when using an undefined variable
 
 # set the output variable release-url to http://example.com
 # echo "::set-output name=release-url::http://example.com" 
 echo "release-url=http://example.com" >> $GITHUB_OUTPUT
-echo "The release-url is: $GITHUB_OUTPUT"
+# echo "The release-url is: $GITHUB_OUTPUT"
 
 # exit with a successful status code
 exit 0 
