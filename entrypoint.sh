cb98fb86cdd7addf6a6e2c073f7b43a3efd28fe7
lolmeida <lolmeida@gmail.com>
2024-04-28 16:51:23 +0100

    added workfloes/test.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,8 @@ set -e -x -u
 
 # set the output variable release-url to http://example.com
 # echo "::set-output name=release-url::http://example.com" 
-echo "release-url=http://example.com" >> $GITHUB_OUTPUT
-# echo "The release-url is: $GITHUB_OUTPUT"
+# echo "release-url=http://example.com" >> $GITHUB_OUTPUT
+echo "The release-url is: BATATA"
 
 # exit with a successful status code
 exit 0 
